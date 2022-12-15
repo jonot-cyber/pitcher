823ef984bbc0ae338bf7a64e01a3293a5b7e834e
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-15 15:44:53 -0500

    load markdown file from path

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"os"
 )
 
 func main() {
@@ -10,4 +13,14 @@ func main() {
     if len(args) != 1 {
         log.Fatalln("You need to select a file!")
     }
+
+    f, err := os.Open(args[0])
+    if err != nil {
+        log.Fatal(err)
+    }
+    markdown, err := io.ReadAll(f)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(markdown)
 }
